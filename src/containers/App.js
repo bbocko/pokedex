08ab91f5cedfa2f92861df73bf98d4944f8954f8
Blaž <blaz.bocko1@gmail.com>
2023-04-08 10:04:27 +0200

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Card from '../components/Card';
+import SearchForm from '../components/SearchForm';
 
 function App() {
   return (
-    <div>
+    <div className='tc'>
       <h1>Pokedex</h1>
+      <h3>Search your favourite pokemon and find more about it!</h3>
+      <SearchForm />
+      <Card />
     </div>
   );
 }
