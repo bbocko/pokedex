7098a52312bfe53760d111eda52dd5e69c511e9b
Blaž <blaz.bocko1@gmail.com>
2023-04-15 17:09:31 +0200

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Card from '../components/Card';
+// import Card from '../components/Card';
 import SearchForm from '../components/SearchForm';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <h1>Pokedex</h1>
       <h3>Search your favourite pokemon and find more about it!</h3>
       <SearchForm />
-      <Card />
+      {/* <Card /> card component needs to be requested via API (implement it on searchbox) */}
     </div>
   );
 }
