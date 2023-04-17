28cb547738c35ccdd838840a553687c23cc2610d
Blaž <blaz.bocko1@gmail.com>
2023-04-17 19:34:05 +0200

    Update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const SearchForm = () => {
-    const handleClick = (e) => {
+    const handleClick = async function (e) {
         e.preventDefault();
     }
     return (
         <div className='mb3'>
-            <form>
-                <input className='ba b--black-20 pa2 mb2 w-100"' type='search' placeholder='Pokemon name or ID' />
+            <form id='searchForm'>
+                <input className='ba b--black-20 pa2 mb2 w-100"' type='text' placeholder='Pokemon name or ID' />
                 <button className='f5 br2 ba ph2 pv2 ml2 dib dark-blue pointer' onClick={handleClick}>Search</button>
             </form>
         </div>
