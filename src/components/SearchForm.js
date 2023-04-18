acc80d1e8d4b5fec48452b6f2067fabd1973b2a6
Blaž <blaz.bocko1@gmail.com>
2023-04-18 21:10:02 +0200

    Update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchForm = () => {
-    const handleClick = async function (e) {
-        e.preventDefault();
+const SearchForm = ({ onSearch }) => {
+    const [pokemonName, setPokemonName] = useState('');
+
+    const handleChange = (e) => {
+        setPokemonName(e.target.value);
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSearch(pokemonName);
+    }
+
     return (
         <div className='mb3'>
-            <form id='searchForm'>
-                <input className='ba b--black-20 pa2 mb2 w-100"' type='text' placeholder='Pokemon name or ID' />
-                <button className='f5 br2 ba ph2 pv2 ml2 dib dark-blue pointer' onClick={handleClick}>Search</button>
+            <form id='searchForm' onSubmit={handleSubmit}>
+                <input
+                    className='ba b--black-20 pa2 mb2 w-100"'
+                    type='text'
+                    placeholder='Pokemon name or ID'
+                    value={pokemonName}
+                    onChange={handleChange}
+                />
+                <button
+                    className='f5 br2 ba ph2 pv2 ml2 dib dark-blue pointer'
+                    type='submit'
+                >
+                    Search
+                </button>
             </form>
         </div>
     );
