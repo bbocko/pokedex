fd36b3bcef2ab5591692f12608ed5f2501210a07
Blaž <blaz.bocko1@gmail.com>
2023-04-18 21:10:04 +0200

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Card from '../components/Card';
 import SearchForm from '../components/SearchForm';
+import axios from 'axios';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState(null);
+
+  const handleSearch = async (pokemonName) => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      const data = response.data;
+      setPokemonData(data);
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error);
+    }
+  }
+
   return (
     <div className='tc'>
       <h1>Pokedex</h1>
       <h3>Search your favourite pokemon and find more about it!</h3>
-      <SearchForm />
-      <Card />
+      <SearchForm onSearch={handleSearch} />
+      {pokemonData && <Card pokemonData={pokemonData} />}
     </div>
   );
 }
