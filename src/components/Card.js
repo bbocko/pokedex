8323bcf825bea15e8052de631e758bfb7278a99e
Blaž <blaz.bocko1@gmail.com>
2023-04-18 21:58:17 +0200

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const Card = ({ pokemonData }) => {
+const Card = ({ pokemonData, speciesData }) => {
     const imageUrl = pokemonData.sprites.other['official-artwork']?.front_default || pokemonData.sprites.front_default;
     const pokemonID = pokemonData.id;
+
     const capitalizeFirstLetter = (str) => {
         return str.charAt(0).toUpperCase() + str.slice(1);
     };
     const pokemonName = capitalizeFirstLetter(pokemonData.name);
+
     const pokemonType = pokemonData.types[0].type.name.toUpperCase();
+
+    const convertToMeters = (height) => {
+        return (height / 10).toFixed(1);
+    };
+    const height = pokemonData.height;
+    const heightInMeters = convertToMeters(height);
+
+    const weight = pokemonData.weight / 10;
+
+    const flavorText = speciesData ? speciesData.flavor_text_entries[9]?.flavor_text || '' : '';
+    const description = flavorText.replace(/POKéMON/g, 'Pokemon')
 
     return (
         <div className="tc bg-washed-red ba dib br3 pa3 ma2 bw1 shadow-5">
@@ -18,11 +31,11 @@ const Card = ({ pokemonData }) => {
             <div>
                 <button className='f4 br3 dib ph2 pv1 bg-light-red'>{pokemonType}</button>
             </div>
-            {/* <div className='tl'>
-                <p>HT {pokemon height}</p>
-                <p>WT {pokemoon weight}</p>
-                <p className='tc lh-copy measure-narrow'>{pokemon description}</p>
-            </div> */}
+            <div className='tl'>
+                <p>HT {heightInMeters} m</p>
+                <p>WT {weight} kg</p>
+                <p className='tc lh-copy'>{description}</p>
+            </div>
         </div>
     );
 }
